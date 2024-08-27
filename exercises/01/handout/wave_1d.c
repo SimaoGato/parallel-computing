8de3f3f5c76917965f64a1b18cea1d04ab1f43a6
Simão Gato <simao.valedegato@gmail.com>
2024-08-27 18:56:20 +0200

    complete code tasks of assignment 1a

--- exercises/01/handout/wave_1d.c
+++ exercises/01/handout/wave_1d.c
@@ -49,9 +49,21 @@ void domain_save ( int_t step )
 // and set the time step.
 void domain_initialize ( void )
 {
-// BEGIN: T1
-    ;
-// END: T1
+    // BEGIN: T1
+    // Allocate memory for the buffers (N + 2 for the ghost points)
+    for (int i = 0; i < 3; ++i) {
+        buffers[i] = (real_t *)malloc((N + 2) * sizeof(real_t));
+    }
+
+    // Initialize the U_prv and U buffers with the initial condition
+    for (int_t i = 0; i < N; ++i) {
+        real_t x = (real_t)i / (real_t)(N - 1);
+        U(i) = U_prv(i) = cos(M_PI * x);
+    }
+
+    // Set the time step based on the CFL condition
+    dt = dx / c;
+    // END: T1
 }
 
 
@@ -60,7 +72,9 @@ void domain_initialize ( void )
 // BEGIN: T2
 void domain_finalize ( void )
 {
-    ;
+    for (int i = 0; i < 3; ++i) {
+        free(buffers[i]);
+    }
 }
 // END: T2
 
@@ -68,21 +82,44 @@ void domain_finalize ( void )
 // TASK: T3
 // Rotate the time step buffers.
 // BEGIN: T3
-    ;
+// TASK T3: Rotate the time step buffers.
+void domain_rotate_buffers(void)
+{
+    real_t *temp = buffers[0];
+    buffers[0] = buffers[1];
+    buffers[1] = buffers[2];
+    buffers[2] = temp;
+}
+
 // END: T3
 
 
 // TASK: T4
 // Derive step t+1 from steps t and t-1.
 // BEGIN: T4
-    ;
+// TASK T4: Derive step t+1 from steps t and t-1.
+void domain_update(void)
+{
+    for (int_t i = 1; i < N; i++)
+    {
+        U_nxt(i) = -U_prv(i) + 2 * U(i) + (dt * dt * c * c / (dx * dx)) * (U(i - 1) + U(i + 1) - 2 * U(i));
+    }
+}
+
 // END: T4
 
 
 // TASK: T5
 // Neumann (reflective) boundary condition.
 // BEGIN: T5
-    ;
+// TASK T5: Neumann (reflective) boundary condition.
+void domain_apply_boundary(void)
+{
+    // Reflect the value at the boundary by mirroring the interior value.
+    U(0) = U(1);       // Left boundary
+    U(N) = U(N-1);   // Right boundary
+}
+
 // END: T5
 
 
@@ -91,8 +128,23 @@ void domain_finalize ( void )
 void simulate( void )
 {
 // BEGIN: T6
-    int_t iteration=0;
-    domain_save ( iteration / snapshot_freq );
+    for (int_t iteration = 0; iteration < max_iteration; iteration++)
+    {
+        // Apply Neumann boundary conditions
+        domain_apply_boundary();
+
+        // Update the wave equation for the next time step
+        domain_update();
+
+        // Rotate the buffers to advance in time
+        domain_rotate_buffers();
+
+        // Save the state at specified intervals
+        if (iteration % snapshot_freq == 0)
+        {
+            domain_save(iteration/snapshot_freq);
+        }
+    }
 // END: T6
 }
 
