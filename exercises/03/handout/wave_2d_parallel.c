f155f1885953755cd1bad1c64569b90a901466c5
Simão Gato <simao.valedegato@gmail.com>
2024-09-25 10:57:19 +0200

    ex03 back to beginning

--- exercises/03/handout/wave_2d_parallel.c
+++ exercises/03/handout/wave_2d_parallel.c
@@ -10,7 +10,10 @@
 
 // TASK: T1a
 // Include the MPI headerfile
-#include <mpi.h>
+// BEGIN: T1a
+;
+// END: T1a
+
 
 // Convert 'struct timeval' into seconds in double prec. floating point
 #define WALLTIME(t) ((double)(t).tv_sec + 1e-6 * (double)(t).tv_usec)
@@ -19,20 +22,18 @@
 typedef int64_t int_t;
 typedef double real_t;
 
+
 // Buffers for three time steps, indexed with 2 ghost points for the boundary
-real_t *buffers[3] = { NULL, NULL, NULL };
+real_t
+    *buffers[3] = { NULL, NULL, NULL };
 
 // TASK: T1b
 // Declare variables each MPI process will need
-int world_size, world_rank;
-int cart_dims[2], cart_periods[2] = {0, 0}, cart_coords[2];
-MPI_Comm cart_comm;
-int local_M, local_N;
-int north, south, east, west;
-
-#define U_prv(i,j) buffers[0][((i)+1)*(local_N+2)+(j)+1]
-#define U(i,j)     buffers[1][((i)+1)*(local_N+2)+(j)+1]
-#define U_nxt(i,j) buffers[2][((i)+1)*(local_N+2)+(j)+1]
+// BEGIN: T1b
+#define U_prv(i,j) buffers[0][((i)+1)*(N+2)+(j)+1]
+#define U(i,j)     buffers[1][((i)+1)*(N+2)+(j)+1]
+#define U_nxt(i,j) buffers[2][((i)+1)*(N+2)+(j)+1]
+// END: T1b
 
 // Simulation parameters: size, step count, and how often to save the state
 int_t
@@ -46,10 +47,14 @@ const real_t
     c  = 1.0,
     dx = 1.0,
     dy = 1.0;
-real_t dt;
+real_t
+    dt;
+
+
+
 
 // Rotate the time step buffers.
-void move_buffer_window(void)
+void move_buffer_window ( void )
 {
     real_t *temp = buffers[0];
     buffers[0] = buffers[1];
@@ -57,65 +62,51 @@ void move_buffer_window(void)
     buffers[2] = temp;
 }
 
+
 // TASK: T4
 // Set up our three buffers, and fill two with an initial perturbation
 // and set the time step.
-void domain_initialize(void)
+void domain_initialize ( void )
 {
-    local_M = M / cart_dims[0];
-    local_N = N / cart_dims[1];
+// BEGIN: T4
+    buffers[0] = malloc ( (M+2)*(N+2)*sizeof(real_t) );
+    buffers[1] = malloc ( (M+2)*(N+2)*sizeof(real_t) );
+    buffers[2] = malloc ( (M+2)*(N+2)*sizeof(real_t) );
 
-    // Initialize neighbour ranks
-    north = south = east = west = MPI_PROC_NULL;
-    MPI_Cart_shift(cart_comm, 0, 1, &north, &south);
-    MPI_Cart_shift(cart_comm, 1, 1, &west, &east);
-
-    // Print the domain decomposition
-    if (world_rank == 0)
+    for ( int_t i=0; i<M; i++ )
     {
-        printf("Decomposition: %dx%d\n", cart_dims[0], cart_dims[1]);
-    }
-
-    printf("Rank %d: local_M = %d, local_N = %d\n", world_rank, local_M, local_N);
-    printf("Rank %d: cart_coords = (%d, %d)\n", world_rank, cart_coords[0], cart_coords[1]);
-    printf("Rank %d: north = %d, south = %d, east = %d, west = %d\n", world_rank, north, south, east, west);
-
-    buffers[0] = malloc((local_M+2)*(local_N+2)*sizeof(real_t));
-    buffers[1] = malloc((local_M+2)*(local_N+2)*sizeof(real_t));
-    buffers[2] = malloc((local_M+2)*(local_N+2)*sizeof(real_t));
-
-    for (int_t i = 0; i < local_M; i++)
-    {
-        for (int_t j = 0; j < local_N; j++)
+        for ( int_t j=0; j<N; j++ )
         {
-            int_t global_i = cart_coords[0] * local_M + i;
-            int_t global_j = cart_coords[1] * local_N + j;
             // Calculate delta (radial distance) adjusted for M x N grid
-            real_t delta = sqrt(((global_i - M/2.0) * (global_i - M/2.0)) / (real_t)M +
-                                ((global_j - N/2.0) * (global_j - N/2.0)) / (real_t)N);
-            U_prv(i,j) = U(i,j) = exp(-4.0*delta*delta);
+            real_t delta = sqrt ( ((i - M/2.0) * (i - M/2.0)) / (real_t)M +
+                                ((j - N/2.0) * (j - N/2.0)) / (real_t)N );
+            U_prv(i,j) = U(i,j) = exp ( -4.0*delta*delta );
         }
     }
 
     // Set the time step for 2D case
-    dt = dx*dy / (c * sqrt(dx*dx+dy*dy));
+    dt = dx*dy / (c * sqrt (dx*dx+dy*dy));
+// END: T4
 }
 
+
 // Get rid of all the memory allocations
-void domain_finalize(void)
+void domain_finalize ( void )
 {
-    free(buffers[0]);
-    free(buffers[1]);
-    free(buffers[2]);
+    free ( buffers[0] );
+    free ( buffers[1] );
+    free ( buffers[2] );
 }
+
 
 // TASK: T5
 // Integration formula
-void time_step(void)
+void time_step ( void )
 {
-    for (int_t i = 0; i < local_M; i++)
+// BEGIN: T5
+    for ( int_t i=0; i<M; i++ )
     {
-        for (int_t j = 0; j < local_N; j++)
+        for ( int_t j=0; j<N; j++ )
         {
             U_nxt(i,j) = -U_prv(i,j) + 2.0*U(i,j)
                      + (dt*dt*c*c)/(dx*dy) * (
@@ -123,106 +114,64 @@ void time_step(void)
                     );
         }
     }
+// END: T5
 }
 
 // TASK: T6
 // Communicate the border between processes.
-void border_exchange(void)
+void border_exchange ( void )
 {
-    MPI_Status status;
-
-    //MPI_Cart_shift(cart_comm, 0, 1, &north, &south);
-    //MPI_Cart_shift(cart_comm, 1, 1, &west, &east);
-
-    // Send to north, receive from south
-    MPI_Sendrecv(&U(0,0), local_N, MPI_DOUBLE, north, 0,
-                 &U(local_M,0), local_N, MPI_DOUBLE, south, 0,
-                 cart_comm, &status);
-
-    // Send to south, receive from north
-    MPI_Sendrecv(&U(local_M-1,0), local_N, MPI_DOUBLE, south, 1,
-                 &U(-1,0), local_N, MPI_DOUBLE, north, 1,
-                 cart_comm, &status);
-
-    // Send to east, receive from west
-    MPI_Sendrecv(&U(0,local_N-1), local_M, MPI_DOUBLE, east, 2,
-                 &U(0,-1), local_M, MPI_DOUBLE, west, 2,
-                 cart_comm, &status);
-
-    // Send to west, receive from east
-    MPI_Sendrecv(&U(0,0), local_M, MPI_DOUBLE, west, 3,
-                 &U(0,local_N), local_M, MPI_DOUBLE, east, 3,
-                 cart_comm, &status);
+// BEGIN: T6
+    ;
+// END: T6
 }
+
 
 // TASK: T7
 // Neumann (reflective) boundary condition
-void boundary_condition(void)
+void boundary_condition ( void )
 {
-    if (south == MPI_PROC_NULL)
+// BEGIN: T7
+    for ( int_t i=0; i<M; i++ )
     {
-        for (int_t j = 0; j < local_N; j++)
-        {
-            U(-1,j) = U(0,j);
-        }
+        U(i,-1) = U(i,1);
+        U(i,N)  = U(i,N-2);
     }
-
-    if (north == MPI_PROC_NULL)
+    for ( int_t j=0; j<N; j++ )
     {
-        for (int_t j = 0; j < local_N; j++)
-        {
-            U(local_M,j) = U(local_M-1,j);
-        }
+        U(-1,j) = U(1,j);
+        U(M,j)  = U(M-2,j);
     }
-
-    if (west == MPI_PROC_NULL)
-    {
-        for (int_t i = 0; i < local_M; i++)
-        {
-            U(i,-1) = U(i,0);
-        }
-    }
-
-    if (east == MPI_PROC_NULL)
-    {
-        for (int_t i = 0; i < local_M; i++)
-        {
-            U(i,local_N) = U(i,local_N-1);
-        }
-    }
+// END: T7
 }
+
 
 // TASK: T8
 // Save the present time step in a numbered file under 'data/'
-void domain_save(int_t step)
+void domain_save ( int_t step )
 {
+// BEGIN: T8
     char filename[256];
-    sprintf(filename, "data/%.5ld.dat", step);
-    MPI_File fh;
-    MPI_File_open(cart_comm, filename, MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
-
-    MPI_Offset offset = (cart_coords[0] * local_M * N + cart_coords[1] * local_N) * sizeof(real_t);
-    printf("Rank %d: offset = %lld\n", world_rank, offset);
-    MPI_File_set_view(fh, offset, MPI_DOUBLE, MPI_DOUBLE, "native", MPI_INFO_NULL);
-
-    for (int_t i = 0; i < local_M; i++)
+    sprintf ( filename, "data/%.5ld.dat", step );
+    FILE *out = fopen ( filename, "wb" );
+    for ( int_t i=0; i<M; i++ )
     {
-        printf("Rank %d: writing row %ld\n", world_rank, i*local_N);
-        MPI_File_write(fh, &U(i,0), local_N, MPI_DOUBLE, MPI_STATUS_IGNORE);
+        fwrite ( &U(i,0), sizeof(real_t), N, out );
     }
-
-    MPI_File_close(&fh);
+    fclose ( out );
+// END: T8
 }
 
+
 // Main time integration.
-void simulate(void)
+void simulate( void )
 {
     // Go through each time step
-    for (int_t iteration = 0; iteration <= max_iteration; iteration++)
+    for ( int_t iteration=0; iteration<=max_iteration; iteration++ )
     {
-        if ((iteration % snapshot_freq) == 0)
+        if ( (iteration % snapshot_freq)==0 )
         {
-            domain_save(iteration / snapshot_freq);
+            domain_save ( iteration / snapshot_freq );
         }
 
         // Derive step t+1 from steps t and t-1
@@ -235,76 +184,52 @@ void simulate(void)
     }
 }
 
-int main(int argc, char **argv)
-{
-    // TASK: T1c
-    // Initialise MPI
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    // TASK: T3
-    // Distribute the user arguments to all the processes
-    OPTIONS *options = NULL;
-    if (world_rank == 0)
-    {
-        options = parse_args(argc, argv);
-        if (!options)
+int main ( int argc, char **argv )
+{
+// TASK: T1c
+// Initialise MPI
+// BEGIN: T1c
+    ;
+// END: T1c
+
+
+// TASK: T3
+// Distribute the user arguments to all the processes
+// BEGIN: T3
+        OPTIONS *options = parse_args( argc, argv );
+        if ( !options )
         {
-            fprintf(stderr, "Argument parsing failed\n");
-            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+            fprintf( stderr, "Argument parsing failed\n" );
+            exit( EXIT_FAILURE );
         }
 
         M = options->M;
         N = options->N;
-        max_iteration = 500;
+        max_iteration = options->max_iteration;
         snapshot_freq = options->snapshot_frequency;
-    }
-
-    MPI_Bcast(&M, 1, MPI_INT64_T, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&N, 1, MPI_INT64_T, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&max_iteration, 1, MPI_INT64_T, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&snapshot_freq, 1, MPI_INT64_T, 0, MPI_COMM_WORLD);
-
-    // Create Cartesian communicator
-    MPI_Dims_create(world_size, 2, cart_dims);
-    MPI_Cart_create(MPI_COMM_WORLD, 2, cart_dims, cart_periods, 0, &cart_comm);
-    MPI_Cart_coords(cart_comm, world_rank, 2, cart_coords);
+// END: T3
 
     // Set up the initial state of the domain
     domain_initialize();
 
+
     struct timeval t_start, t_end;
 
-    // TASK: T2
-    // Time your code
-    MPI_Barrier(cart_comm);
-    if (world_rank == 0)
-    {
-        gettimeofday(&t_start, NULL);
-    }
-
+// TASK: T2
+// Time your code
+// BEGIN: T2
     simulate();
-
-    MPI_Barrier(cart_comm);
-    if (world_rank == 0)
-    {
-        gettimeofday(&t_end, NULL);
-        double elapsed = WALLTIME(t_end) - WALLTIME(t_start);
-        printf("Elapsed time: %f seconds\n", elapsed);
-    }
+// END: T2
 
     // Clean up and shut down
     domain_finalize();
 
-    // TASK: T1d
-    // Finalise MPI
-    MPI_Finalize();
+// TASK: T1d
+// Finalise MPI
+// BEGIN: T1d
+    ;
+// END: T1d
 
-    if (world_rank == 0 && options)
-    {
-        free(options);
-    }
-
-    return EXIT_SUCCESS;
+    exit ( EXIT_SUCCESS );
 }
