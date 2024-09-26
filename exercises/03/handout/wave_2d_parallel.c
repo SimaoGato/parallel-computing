3d51e494e824ebfa7163c880d581697d9384a529
Simão Gato <simao.valedegato@gmail.com>
2024-09-26 16:02:03 +0200

    do ex03 task 2

--- exercises/03/handout/wave_2d_parallel.c
+++ exercises/03/handout/wave_2d_parallel.c
@@ -25,7 +25,7 @@ typedef double real_t;
 
 // Buffers for three time steps, indexed with 2 ghost points for the boundary
 real_t
-    *buffers[3] = { NULL, NULL, NULL };
+*buffers[3] = { NULL, NULL, NULL };
 
 // TASK: T1b
 // Declare variables each MPI process will need
@@ -42,18 +42,18 @@ int local_M, local_N;
 
 // Simulation parameters: size, step count, and how often to save the state
 int_t
-    M = 256,    // rows
-    N = 256,    // cols
-    max_iteration = 4000,
-    snapshot_freq = 20;
+M = 256,    // rows
+N = 256,    // cols
+max_iteration = 4000,
+snapshot_freq = 20;
 
 // Wave equation parameters, time step is derived from the space step
 const real_t
-    c  = 1.0,
-    dx = 1.0,
-    dy = 1.0;
+c  = 1.0,
+dx = 1.0,
+dy = 1.0;
 real_t
-    dt;
+dt;
 
 
 
@@ -73,7 +73,7 @@ void move_buffer_window ( void )
 // and set the time step.
 void domain_initialize ( void )
 {
-// BEGIN: T4
+    // BEGIN: T4
     buffers[0] = malloc ( (M+2)*(N+2)*sizeof(real_t) );
     buffers[1] = malloc ( (M+2)*(N+2)*sizeof(real_t) );
     buffers[2] = malloc ( (M+2)*(N+2)*sizeof(real_t) );
@@ -84,14 +84,14 @@ void domain_initialize ( void )
         {
             // Calculate delta (radial distance) adjusted for M x N grid
             real_t delta = sqrt ( ((i - M/2.0) * (i - M/2.0)) / (real_t)M +
-                                ((j - N/2.0) * (j - N/2.0)) / (real_t)N );
+                                 ((j - N/2.0) * (j - N/2.0)) / (real_t)N );
             U_prv(i,j) = U(i,j) = exp ( -4.0*delta*delta );
         }
     }
 
     // Set the time step for 2D case
     dt = dx*dy / (c * sqrt (dx*dx+dy*dy));
-// END: T4
+    // END: T4
 }
 
 
@@ -108,27 +108,27 @@ void domain_finalize ( void )
 // Integration formula
 void time_step ( void )
 {
-// BEGIN: T5
+    // BEGIN: T5
     for ( int_t i=0; i<M; i++ )
     {
         for ( int_t j=0; j<N; j++ )
         {
             U_nxt(i,j) = -U_prv(i,j) + 2.0*U(i,j)
-                     + (dt*dt*c*c)/(dx*dy) * (
-                        U(i-1,j)+U(i+1,j)+U(i,j-1)+U(i,j+1)-4.0*U(i,j)
-                    );
+                + (dt*dt*c*c)/(dx*dy) * (
+                U(i-1,j)+U(i+1,j)+U(i,j-1)+U(i,j+1)-4.0*U(i,j)
+            );
         }
     }
-// END: T5
+    // END: T5
 }
 
 // TASK: T6
 // Communicate the border between processes.
 void border_exchange ( void )
 {
-// BEGIN: T6
+    // BEGIN: T6
     ;
-// END: T6
+    // END: T6
 }
 
 
@@ -136,7 +136,7 @@ void border_exchange ( void )
 // Neumann (reflective) boundary condition
 void boundary_condition ( void )
 {
-// BEGIN: T7
+    // BEGIN: T7
     for ( int_t i=0; i<M; i++ )
     {
         U(i,-1) = U(i,1);
@@ -147,7 +147,7 @@ void boundary_condition ( void )
         U(-1,j) = U(1,j);
         U(M,j)  = U(M-2,j);
     }
-// END: T7
+    // END: T7
 }
 
 
@@ -155,7 +155,7 @@ void boundary_condition ( void )
 // Save the present time step in a numbered file under 'data/'
 void domain_save ( int_t step )
 {
-// BEGIN: T8
+    // BEGIN: T8
     char filename[256];
     sprintf ( filename, "data/%.5ld.dat", step );
     FILE *out = fopen ( filename, "wb" );
@@ -164,7 +164,7 @@ void domain_save ( int_t step )
         fwrite ( &U(i,0), sizeof(real_t), N, out );
     }
     fclose ( out );
-// END: T8
+    // END: T8
 }
 
 
@@ -192,30 +192,30 @@ void simulate( void )
 
 int main ( int argc, char **argv )
 {
-// TASK: T1c
-// Initialise MPI
-// BEGIN: T1c
+    // TASK: T1c
+    // Initialise MPI
+    // BEGIN: T1c
     MPI_Init( &argc, &argv );
     MPI_Comm_size( MPI_COMM_WORLD, &world_size );
     MPI_Comm_rank( MPI_COMM_WORLD, &world_rank );
-// END: T1c
+    // END: T1c
 
 
-// TASK: T3
-// Distribute the user arguments to all the processes
-// BEGIN: T3
-        OPTIONS *options = parse_args( argc, argv );
-        if ( !options )
-        {
-            fprintf( stderr, "Argument parsing failed\n" );
-            exit( EXIT_FAILURE );
-        }
+    // TASK: T3
+    // Distribute the user arguments to all the processes
+    // BEGIN: T3
+    OPTIONS *options = parse_args( argc, argv );
+    if ( !options )
+    {
+        fprintf( stderr, "Argument parsing failed\n" );
+        exit( EXIT_FAILURE );
+    }
 
-        M = options->M;
-        N = options->N;
-        max_iteration = options->max_iteration;
-        snapshot_freq = options->snapshot_frequency;
-// END: T3
+    M = options->M;
+    N = options->N;
+    max_iteration = options->max_iteration;
+    snapshot_freq = options->snapshot_frequency;
+    // END: T3
 
     // Set up the initial state of the domain
     domain_initialize();
@@ -223,20 +223,32 @@ int main ( int argc, char **argv )
 
     struct timeval t_start, t_end;
 
-// TASK: T2
-// Time your code
-// BEGIN: T2
+    // TASK: T2
+    // Time your code
+    // BEGIN: T2
+    MPI_Barrier(cart_comm); // Ensure all processes start timing together
+    gettimeofday(&t_start, NULL);
+
     simulate();
-// END: T2
+
+    MPI_Barrier(cart_comm); // Ensure all processes finish timing together
+    gettimeofday(&t_end, NULL);
+
+    if (world_rank == 0)
+    {
+        double elapsed = WALLTIME(t_end) - WALLTIME(t_start);
+        printf("Simulation completed in %.6f seconds\n", elapsed);
+    }
+    // END: T2
 
     // Clean up and shut down
     domain_finalize();
 
-// TASK: T1d
-// Finalise MPI
-// BEGIN: T1d
+    // TASK: T1d
+    // Finalise MPI
+    // BEGIN: T1d
     MPI_Finalize();
-// END: T1d
+    // END: T1d
 
     exit ( EXIT_SUCCESS );
 }
